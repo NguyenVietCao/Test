18c1c8b78becb720b6b6840e3e14f50efcb105c1
NguyenVietCao <vietcao2510@gmail.com>
2023-11-14 15:52:28 +0700

    commit lan dau

---
+++ src/components/LoginSignup/LoginSignup.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './LoginSignup.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function LoginSignup(props) {
+    const [action, setAction] = useState("Login")
+    return (
+        <div className='container'>
+            <div className='header'>
+                <div className='text'>{ action}</div>
+                <div className='underline'></div>
+            </div>
+            <div className='inputs'>
+                {action === "Login" ? <div></div> : <div className='input'>
+                    <image srs="" alt="" ><i className="far fa-user"></i></image>
+                    <input type='text' placeholder='Name' />
+                </div>}
+              
+                <div className='input'>
+                    <image srs="" alt="" ><i className="far fa-envelope"></i></image>
+                    <input type='email' placeholder='Email'/>
+                </div>
+                <div className='input'>
+                    <image srs="" alt="" ><i className="fal fa-lock"></i></image>
+                    <input type='password' placeholder='Password'/>
+                </div>
+                {action === "Sign Up" ? <div></div> : <div className='forgot-password'>Lost Password? <span>Click here!</span></div>}
+              
+                <div className='submit-container' >
+                    <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => {setAction("Sign Up")}}>Sign Up</div>
+                    <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => { setAction("Login")}}>Login</div>
+                </div>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default LoginSignup;
